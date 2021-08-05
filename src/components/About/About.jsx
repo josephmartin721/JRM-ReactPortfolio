648f74237056a306270ad4ecc61c473cf892d83d
josephmartin721 <josephmartin721@yahoo.com>
2021-08-05 15:51:59 -0500

    remove lorem ipsum

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,14 +39,15 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'I graduated from the University of Texas at San Antonio with a Bachelor Degree in Ahtropology.'}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'I have recently my certification in full-stack web devlopment from the UTSA Bootcamp program.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree ||
+                    'I hope to combine both of these skill-sets in order to prove myself as a useful asset for your company.'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
